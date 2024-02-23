5a39130888f72287278540af98ab548aa7549de3
Julian Passebecq <julianpassebecq@gmail.com>
2024-02-23 18:25:47 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -10,19 +10,17 @@ def main():
 
     df = load_data('sitemap_data4_c4.csv')
 
-    # Filter options
-    year = st.selectbox('Select Year', options=df['Year'].unique())
-    website_origin = st.multiselect('Select Website Origin', options=df['Website Origin'].unique())
+    # Multi-select filters for year and website origin
+    selected_years = st.multiselect('Select Year(s)', options=df['Year'].unique())
+    selected_origins = st.multiselect('Select Website Origin(s)', options=df['Website Origin'].unique())
 
-    # Search bar
-    search_query = st.text_input('Search by Article Title')
+    # Filter the DataFrame based on selections
+    if selected_years:
+        df = df[df['Year'].isin(selected_years)]
+    if selected_origins:
+        df = df[df['Website Origin'].isin(selected_origins)]
 
-    # Applying filters
-    filtered_df = df[df['Year'] == year] if year else df
-    filtered_df = filtered_df[filtered_df['Website Origin'].isin(website_origin)] if website_origin else filtered_df
-    filtered_df = filtered_df[filtered_df['Article Title'].str.contains(search_query, case=False)] if search_query else filtered_df
-
-    st.dataframe(filtered_df, use_container_width=True)
+    st.dataframe(df, use_container_width=True)
 
 if __name__ == "__main__":
     main()
