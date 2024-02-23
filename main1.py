bfbb378084f719a885b5718f700b73d9c338526b
Julian Passebecq <julianpassebecq@gmail.com>
2024-02-23 15:40:00 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -26,15 +26,23 @@ def parse_sitemap(url, is_brunner_format=False):
         return []
 
     try:
-        # Namespace handling for standard sitemaps
-        namespace = '' if not is_brunner_format else {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
         sitemap_xml = ET.fromstring(response.content)
         articles = []
-        url_tag = 'url' if not is_brunner_format else 'ns:url'
-        
+
+        if is_brunner_format:
+            namespace = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+            url_tag = 'ns:url'
+            loc_tag = 'ns:loc'
+            lastmod_tag = 'ns:lastmod'
+        else:
+            namespace = {}
+            url_tag = 'url'
+            loc_tag = 'loc'
+            lastmod_tag = 'lastmod'
+
         for url_elem in sitemap_xml.findall(url_tag, namespace):
-            loc = url_elem.find('loc' if not is_brunner_format else 'ns:loc', namespace).text
-            lastmod_elem = url_elem.find('lastmod' if not is_brunner_format else 'ns:lastmod', namespace)
+            loc = url_elem.find(loc_tag, namespace).text
+            lastmod_elem = url_elem.find(lastmod_tag, namespace)
             lastmod = lastmod_elem.text if lastmod_elem is not None else 'Not provided'
             if is_brunner_format and lastmod != 'Not provided':
                 try:
@@ -55,28 +63,29 @@ def main():
     for website, url in standard_sitemap_urls.items():
         articles = parse_sitemap(url)
         for article in articles:
-            article['Website'] = website  # Add website name to each article
+            article['Website'] = website
         all_articles.extend(articles)
 
     # Process Brunner format sitemaps
     for website, url in brunner_sitemap_urls.items():
         articles = parse_sitemap(url, is_brunner_format=True)
         for article in articles:
-            article['Website'] = website  # Add website name to each article
+            article['Website'] = website
         all_articles.extend(articles)
 
     if all_articles:
         articles_df = pd.DataFrame(all_articles)
         articles_df['Last Modified'] = pd.to_datetime(articles_df['Last Modified'], errors='coerce', utc=True)
-        
+
         # Multi-select filter for websites
-        website_selection = st.multiselect('Select Websites', options=list(standard_sitemap_urls.keys()) + list(brunner_sitemap_urls.keys()), default=list(standard_sitemap_urls.keys()) + list(brunner_sitemap_urls.keys()))
+        website_options = list(standard_sitemap_urls.keys()) + list(brunner_sitemap_urls.keys())
+        website_selection = st.multiselect('Select Websites', options=website_options, default=website_options)
         filtered_df = articles_df[articles_df['Website'].isin(website_selection)]
-        
+
         search_query = st.text_input('Enter search term:')
         if search_query:
             filtered_df = filtered_df[filtered_df['URL'].str.contains(search_query, case=False, na=False)]
-        
+
         st.write(filtered_df)
     else:
         st.error("No articles were found. Please check the sitemap URLs.")
