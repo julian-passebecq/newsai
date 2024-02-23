e2fd4f3be5b12e22e8ca789809a5f93a2e69830a
Julian Passebecq <julianpassebecq@gmail.com>
2024-02-23 15:35:21 +0100

    4

--- main1.py
+++ main1.py
@@ -5,56 +5,38 @@ from dateutil import parser
 import pandas as pd
 
 # Define the sitemap URLs for standard and Brunner format
-standard_sitemap_urls = [
-    
-]
+standard_sitemap_urls = {
+    'Kevin R Chant': 'https://www.kevinrchant.com/post-sitemap.xml',
+    'Data Mozart': 'https://data-mozart.com/post-sitemap.xml',
+    'Crossjoin': 'https://blog.crossjoin.co.uk/sitemap-1.xml',
+    'Thomas Leblanc': 'https://thomas-leblanc.com/sitemap-1.xml',
+    # Add more standard sitemaps here
+}
 
-brunner_sitemap_urls = [
-    'https://en.brunner.bi/blog-posts-sitemap.xml',  # Add Brunner format sitemap URLs here
-]
+brunner_sitemap_urls = {
+    'Brunner BI': 'https://en.brunner.bi/blog-posts-sitemap.xml',
+    # Add more Brunner format sitemaps here
+}
 
-def parse_standard_sitemap(url):
-    """Parse sitemaps with the standard format."""
+def parse_sitemap(url, is_brunner_format=False):
+    """Parse sitemaps with the standard or Brunner format."""
     response = requests.get(url)
     if response.status_code != 200:
         st.error(f"Error fetching the sitemap from {url}: HTTP {response.status_code}")
         return []
 
     try:
-        sitemap_xml = ET.fromstring(response.content)
-    except ET.ParseError as e:
-        st.error(f"XML parsing error for {url}: {e}")
-        return []
-
-    namespace = ''
-    if '}' in sitemap_xml.tag:
-        namespace = sitemap_xml.tag.split('}')[0] + '}'
-
-    articles = []
-    for url_elem in sitemap_xml.findall(f'.//{namespace}url'):
-        loc = url_elem.find(f'{namespace}loc').text
-        lastmod_elem = url_elem.find(f'{namespace}lastmod')
-        lastmod = lastmod_elem.text if lastmod_elem is not None else 'Not provided'
-        articles.append({'URL': loc, 'Last Modified': lastmod})
-    return articles
-
-def parse_brunner_sitemap(url):
-    """Parse sitemaps with the Brunner format."""
-    response = requests.get(url)
-    if response.status_code != 200:
-        st.error(f"Error fetching the sitemap from {url}: HTTP {response.status_code}")
-        return []
-
-    try:
-        # Define the namespace
-        namespace = {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
+        # Namespace handling for standard sitemaps
+        namespace = '' if not is_brunner_format else {'ns': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
         sitemap_xml = ET.fromstring(response.content)
         articles = []
-        for url_elem in sitemap_xml.findall('ns:url', namespace):
-            loc = url_elem.find('ns:loc', namespace).text
-            lastmod_elem = url_elem.find('ns:lastmod', namespace)
+        url_tag = 'url' if not is_brunner_format else 'ns:url'
+        
+        for url_elem in sitemap_xml.findall(url_tag, namespace):
+            loc = url_elem.find('loc' if not is_brunner_format else 'ns:loc', namespace).text
+            lastmod_elem = url_elem.find('lastmod' if not is_brunner_format else 'ns:lastmod', namespace)
             lastmod = lastmod_elem.text if lastmod_elem is not None else 'Not provided'
-            if lastmod != 'Not provided':
+            if is_brunner_format and lastmod != 'Not provided':
                 try:
                     lastmod = parser.parse(lastmod).isoformat()
                 except ValueError:
@@ -70,24 +52,32 @@ def main():
 
     all_articles = []
     # Process standard format sitemaps
-    for url in standard_sitemap_urls:
-        articles = parse_standard_sitemap(url)
+    for website, url in standard_sitemap_urls.items():
+        articles = parse_sitemap(url)
+        for article in articles:
+            article['Website'] = website  # Add website name to each article
         all_articles.extend(articles)
 
     # Process Brunner format sitemaps
-    for url in brunner_sitemap_urls:
-        articles = parse_brunner_sitemap(url)
+    for website, url in brunner_sitemap_urls.items():
+        articles = parse_sitemap(url, is_brunner_format=True)
+        for article in articles:
+            article['Website'] = website  # Add website name to each article
         all_articles.extend(articles)
 
     if all_articles:
         articles_df = pd.DataFrame(all_articles)
         articles_df['Last Modified'] = pd.to_datetime(articles_df['Last Modified'], errors='coerce', utc=True)
+        
+        # Multi-select filter for websites
+        website_selection = st.multiselect('Select Websites', options=list(standard_sitemap_urls.keys()) + list(brunner_sitemap_urls.keys()), default=list(standard_sitemap_urls.keys()) + list(brunner_sitemap_urls.keys()))
+        filtered_df = articles_df[articles_df['Website'].isin(website_selection)]
+        
         search_query = st.text_input('Enter search term:')
         if search_query:
-            filtered_articles = articles_df[articles_df['URL'].str.contains(search_query, case=False, na=False)]
-            st.write(filtered_articles)
-        else:
-            st.write(articles_df)
+            filtered_df = filtered_df[filtered_df['URL'].str.contains(search_query, case=False, na=False)]
+        
+        st.write(filtered_df)
     else:
         st.error("No articles were found. Please check the sitemap URLs.")
 
