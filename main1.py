508ea787e2281ad825310f06497b37046d35d9c5
Julian Passebecq <julianpassebecq@gmail.com>
2024-02-23 11:27:03 +0100

    Update main1.py

--- main1.py
+++ main1.py
@@ -9,7 +9,8 @@ sitemap_urls = {
     'Kevin R Chant': 'https://www.kevinrchant.com/post-sitemap.xml',
     'Data Mozart': 'https://data-mozart.com/post-sitemap.xml',
     'Crossjoin': 'https://blog.crossjoin.co.uk/sitemap-1.xml',
-    'Thomas Leblanc': 'https://thomas-leblanc.com/sitemap-1.xml'
+    'Thomas Leblanc': 'https://thomas-leblanc.com/sitemap-1.xml',
+    # Add more sitemaps here as needed
 }
 
 def parse_sitemap(url):
@@ -29,8 +30,13 @@ def parse_sitemap(url):
     articles = []
     for url_elem in sitemap_xml.findall(f'.//{namespace}url'):
         loc = url_elem.find(f'{namespace}loc').text if url_elem.find(f'{namespace}loc') is not None else 'URL not found'
-        lastmod_elem = url_elem.find(f'{namespace}lastmod')
-        lastmod = lastmod_elem.text if lastmod_elem is not None else 'Not provided'
+        lastmod_tags = [f'{namespace}lastmod', f'{namespace}LastMod', f'{namespace}Last Modified']  # Add more variations as needed
+        lastmod = 'Not provided'
+        for tag in lastmod_tags:
+            lastmod_elem = url_elem.find(tag)
+            if lastmod_elem is not None:
+                lastmod = lastmod_elem.text
+                break
         articles.append({'URL': loc, 'Last Modified': lastmod})
 
     return articles
