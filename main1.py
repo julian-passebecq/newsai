e674299b10fda405ac6a356c2f255ad2cb6f3530
Julian Passebecq <julianpassebecq@gmail.com>
2024-02-23 18:03:26 +0100

    2

--- main1.py
+++ main1.py
@@ -1,40 +1,39 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-# Load the CSV data into a DataFrame
-@st.cache
+# Updated function to load data using Streamlit's new caching command
+@st.experimental_memo
 def load_data(csv_file):
     data = pd.read_csv(csv_file)
     return data
 
-# Function to make URLs clickable
-def make_clickable(url, text):
-    return f'<a target="_blank" href="{url}">{text}</a>'
-
-# Streamlit app layout
 def main():
     st.title("Streamlit App for Displaying Sitemap Data")
 
-    # Load and cache the data
-    csv_file = 'sitemap_data4_c4.csv'
+    # Path to the CSV file
+    csv_file = 'sitemap_data_cleaned.csv'
+    
+    # Load the data
     df = load_data(csv_file)
 
-    # Make the URLs clickable
-    df['URL'] = df.apply(lambda row: make_clickable(row['URL'], row['Article Title']), axis=1)
-    
     # Display a search bar and filter data based on user input
     search_query = st.text_input("Search by Article Title:")
     if search_query:
         df = df[df['Article Title'].str.contains(search_query, case=False, na=False)]
     
-    # Multi-select box for filtering by website
-    website_options = df['Website Origin'].unique().tolist()
-    websites_selected = st.multiselect("Filter by Website Origin:", options=website_options, default=website_options)
-    df = df[df['Website Origin'].isin(websites_selected)]
-    
-    # Display the DataFrame
-    st.markdown(df.to_html(escape=False, index=False), unsafe_allow_html=True)
+    # Multi-select box for filtering by website origin
+    websites = df['Website Origin'].unique().tolist()
+    selected_websites = st.multiselect("Filter by Website Origin:", websites, default=websites)
+    df_filtered = df[df['Website Origin'].isin(selected_websites)]
+
+    # Make URLs clickable
+    def make_clickable(name, link):
+        return f'<a target="_blank" href="{link}">{name}</a>'
+
+    df_filtered['clickable_urls'] = df_filtered.apply(lambda x: make_clickable(x['Article Title'], x['URL']), axis=1)
+
+    # Convert DataFrame to HTML for clickable links and display using st.markdown
+    st.markdown(df_filtered.to_html(escape=False, columns=['Last Modified', 'Website Origin', 'Year', 'clickable_urls'], index=False), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
